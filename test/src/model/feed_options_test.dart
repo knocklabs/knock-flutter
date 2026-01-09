bd80a16d63a9d6f097829015e43d81e950e6e991
chris bell <cjbell1988@gmail.com>
2026-01-09 11:50:59 -0500

    chore: bring over latest feed filtering improvements

--- test/src/model/feed_options_test.dart
+++ test/src/model/feed_options_test.dart
@@ -24,13 +24,15 @@ void main() {
         'source': null,
         'tenant': null,
         'has_tenant': null,
+        'workflow_categories': null,
         'archived': 'exclude',
         'trigger_data': null,
+        'inserted_at_date_range': null,
       },
     );
   });
 
-  test('FeedOptions correctly serializes all default fields', () {
+  test('FeedOptions correctly serializes all provided fields', () {
     final options = FeedOptions.defaultOptions().copyWith(
       before: 'test before',
       after: 'test after',
@@ -39,8 +41,14 @@ void main() {
       source: 'test source',
       tenant: 'test tenant',
       hasTenant: true,
+      workflowCategories: ['category1', 'category2'],
       archived: FeedOptionsArchivedScope.include,
       triggerData: {'test_key': 'test_value'},
+      insertedAtDateRange: const InsertedAtDateRange(
+        start: '2024-01-01T00:00:00Z',
+        end: '2024-12-31T23:59:59Z',
+        inclusive: true,
+      ),
     );
     expect(
       options.toJson(),
@@ -52,9 +60,43 @@ void main() {
         'source': 'test source',
         'tenant': 'test tenant',
         'has_tenant': true,
+        'workflow_categories': ['category1', 'category2'],
         'archived': 'include',
         'trigger_data': {'test_key': 'test_value'},
+        'inserted_at_date_range': {
+          'start': '2024-01-01T00:00:00Z',
+          'end': '2024-12-31T23:59:59Z',
+          'inclusive': true,
+        },
       },
     );
+  });
+
+  test('InsertedAtDateRange correctly serializes', () {
+    const dateRange = InsertedAtDateRange(
+      start: '2024-01-01T00:00:00Z',
+      end: '2024-12-31T23:59:59Z',
+      inclusive: true,
+    );
+    expect(
+      dateRange.toJson(),
+      {
+        'start': '2024-01-01T00:00:00Z',
+        'end': '2024-12-31T23:59:59Z',
+        'inclusive': true,
+      },
+    );
+  });
+
+  test('InsertedAtDateRange correctly deserializes', () {
+    final json = {
+      'start': '2024-01-01T00:00:00Z',
+      'end': '2024-12-31T23:59:59Z',
+      'inclusive': true,
+    };
+    final dateRange = InsertedAtDateRange.fromJson(json);
+    expect(dateRange.start, '2024-01-01T00:00:00Z');
+    expect(dateRange.end, '2024-12-31T23:59:59Z');
+    expect(dateRange.inclusive, true);
   });
 }
